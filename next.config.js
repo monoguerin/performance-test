6a2a2e138c13a2cb406074ea5131f1934ae39237
Cesar Guerrero <cesar.guerrero@morningstar.com>
2019-07-09 08:05:00 -0500

    jest config, sass config, bundle analyzer

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withBundleAnalyzer(withSass({
+  cssModules: true,
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
+    },
+  },
+}));
